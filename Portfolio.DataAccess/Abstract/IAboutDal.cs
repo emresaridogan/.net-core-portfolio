75dee242e67fae134b8ee5f23919f06df588125a
emresaridogan <yesaridogan@gmail.com>
2023-03-08 01:04:53 +0300

    model interfaces and ef classes created

---
+++ Portfolio.DataAccess/Abstract/IAboutDal.cs
@@ -0,0 +1,14 @@
+﻿using Portfolio.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.DataAccess.Abstract
+{
+    public interface IAboutDal : IGenericDal<About>
+    {
+      
+    }
+}
---
+++ Portfolio.DataAccess/EntityFramework/EfSkillDal.cs
@@ -0,0 +1,15 @@
+﻿using Portfolio.DataAccess.Abstract;
+using Portfolio.DataAccess.Repository;
+using Portfolio.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.DataAccess.EntityFramework
+{
+    public class EfSkillDal : GenericRepository<Skill>, ISkillDal
+    {
+    }
+}
