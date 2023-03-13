5f45fbdd85b2a321d37596979274b3040f385994
emresaridogan <yesaridogan@gmail.com>
2023-03-13 23:36:55 +0300

    admin theme added and admin controller created

---
+++ portfolio/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Presentetion.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }        
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        } 
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScripts()
+        {
+            return PartialView();
+        }public PartialViewResult PartialNavigation()
+        {
+            return PartialView();
+        }
+    }
+}
