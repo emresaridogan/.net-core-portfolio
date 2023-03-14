11bbe9be749052b9da80ac54aefcf62849277520
emresaridogan <yesaridogan@gmail.com>
2023-03-14 23:42:49 +0300

    introduction controller and views created

---
+++ portfolio/Controllers/IntroductionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Presentetion.Controllers
+{
+    public class IntroductionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
